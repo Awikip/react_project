d6b35f6df8a06e6ba3bd6728df7e48552a08eeb0
Alex <adienaar@conclusion.nl>
2019-03-04 16:22:59 +0100

    added userboard

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -4,14 +4,29 @@ export interface IUserProps {
   name: string;
 }
 
+export interface IPresence {
+  present: boolean;
+}
+
 export default class User extends React.Component<IUserProps> {
+  public state: IPresence = { present: true };
   public render() {
     return (
       <div>
         <p>
           User: <b>{this.props.name}</b>
+          {this.props.name} is {!this.state.present ? 'not' : ''} present
         </p>
+        <button onClick={this.TogglePresence}>
+          {this.state.present ? 'Mark absent' : 'Mark present'}
+        </button>
       </div>
     );
   }
+
+  private TogglePresence = () => {
+    this.setState({
+      presence: !this.state.present,
+    });
+  };
 }
